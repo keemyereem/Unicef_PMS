76f918b03ad476a560fd629c2e54e277ddd63432
WooHaem <whm1001@4-m.co.kr>
2022-08-02 14:08:14 +0900

    backup_previusWork

---
+++ script_welcome/main.js
@@ -0,0 +1,10 @@
+$('.one-time').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000
+});
