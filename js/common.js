7d50cf41238577a93f387bd05c176d1a5245a66b
publisher4m <whm1001@4-m.co.kr>
2022-08-30 18:30:23 +0900

    220830

--- js/common.js
+++ js/common.js
@@ -8,28 +8,22 @@
 // ------------------------------------------------------------------------------------------------------------------------------------------ //
 $(document).ready(function(){
 
-    $('#fullPage').fullpage({
-        sectionSelector: '.slide-box',
-        fitToSection: true,
-        scrollOverflow: true,
-        dragAndMove: true,
-        scrollHorizontally: true,
-        continuousVertical: true,
-
-    });
-
-
     $('.swipe-box').addClass('on');
 
     if ($('.swipe-box').hasClass('on')) {
         setTimeout(() => {
+            $('.icon-hand').addClass('on');
+        }, 0)
+        setTimeout(() => {
             $('.swipe-box').removeClass('on');
-        }, 3000)
+            $('.icon-hand').removeClass('on');
+        }, 2000)
     }
 
     $('.swipe-box').on('click', () => {
         $('.swipe-box').removeClass('on');
     })
+
 
 
 })
